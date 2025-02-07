e88cd289ed588ab9a7fb04c6a08a1d2bedb5d771
leraspizh <gonine38@gmail.com>
2025-02-07 13:55:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TELEGRAM_BOT_TOKEN = " "
-API_TOKEN_SANDBOX= " "
+API_TOKEN_SANDBOX= "e4oEaZY1Kom5OXzybETkMlwjOCy3i8GSCGTHzWrhd4dc563b"
 
 API_URL = "http://127.0.0.1:8000/api/check-imei"
 SERVICES_URL = "https://api.imeicheck.net/v1/services"
